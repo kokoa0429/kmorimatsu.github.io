a5f19ef8ca453c5ebe46c94f76b3d7c88b2c61d3
Katsumi <kmorimatsu@users.sourceforge.jp>
2020-09-08 13:57:59 -0700

    Include index.html etc

---
+++ machikania/links.js
@@ -0,0 +1,40 @@
+/************************************
+* MachiKania web written by Katsumi *
+* License of this file is NOT LGPL, *
+* because this contains executable  *
+* of MachiKania Type M system.      *
+************************************/
+
+document.write((function(){/*
+<p><a href="http://www.ze.em-net.ne.jp/~kenken/machikania/typem.html" onclick="window.open(this.href); return false;">MachiKania type M メインページ</a></p>
+<p>MachiKania web BASIC
+<ul>
+<li><a href="./machikania.html">USキーボード 通常画面</a></li>
+<li><a href="./machikania.html?jp=1">JPキーボード 通常画面</a></li>
+<li><a href="./machikaniawide.html">USキーボード 大画面</a></li>
+<li><a href="./machikaniawide.html?jp=1">JPキーボード 大画面</a></li>
+<li><a href="./help.txt" onclick="window.open(this.href); return false;">ヘルプファイル</a></li>
+<li><a href="./class.txt" onclick="window.open(this.href); return false;">クラスの使い方</a></li>
+</ul>
+</p>
+<p>MachiKania web アプリケーション
+<ul>
+<li><a href="./packman.html">パックマン 通常画面</a></li>
+<li><a href="./tetris.html">テトリス 通常画面</a></li>
+<li><a href="./hakomusu.html">箱入り娘 通常画面</a></li>
+<li><a href="./breakout.html">ブロック崩し 通常画面</a></li>
+<li><a href="./packmanwide.html">パックマン 大画面</a></li>
+<li><a href="./tetriswide.html">テトリス 大画面</a></li>
+<li><a href="./hakomusuwide.html">箱入り娘 大画面</a></li>
+<li><a href="./breakoutwide.html">ブロック崩し 大画面</a></li>
+</ul>
+</p>
+<p>MachiKania 開発者のサイト
+<ul>
+<li><a href="http://www.ze.em-net.ne.jp/~kenken/index.html"onclick="window.open(this.href); return false;">趣味の電子工作とプログラミング　ケンケンのホームページ</a></li>
+<li><a href="https://www.recfor.net/jeans/"onclick="window.open(this.href); return false;">Jeans & Development</a></li>
+<li><a href="https://twitter.com/KenKenMkIISR"onclick="window.open(this.href); return false;">Twitter: KenKenMKIISR</a></li>
+<li><a href="https://twitter.com/kats_me"onclick="window.open(this.href); return false;">Twitter: Katsumi</a></li>
+</ul>
+</p>
+*/}).toString().match(/\/\*([\s\S]*)\*\//)[1]);
